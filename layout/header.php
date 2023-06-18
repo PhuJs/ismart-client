1791eae3889a2f5e429c0b65c35aff54d9ee5dd6
{Thanh Phu} <{phuit2000@gmail.com}>
2023-06-18 22:43:59 +0700

    upload header 2

--- layout/header.php
+++ layout/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <base href="https://github.com/PhuJs/ismart-client/">
+    <base href="http://localhost/phpmaster/project/ismart-client/">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
     <link
